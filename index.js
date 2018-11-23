1aa4d51aa8ba22b757d978646c2508b4dc163bbd
Mahendra Hegde <hegde.mahendra@tcs.com>
2018-11-23 14:28:38 +0530

    resolved node double count issue

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const http = require('http');
 var redis = require("redis"),
     client = redis.createClient(6379, 'redis');
 http.createServer(function (req, res) {
-  client.incr("hit",(err,num)=>{
-    res.write(`page hit count is  : ${Math.round(num/2)} `); //write a response to the client
-    res.end(); //end the response
-  })
- 
+  if(req.url=='/'){
+    client.incr("hit",(err,num)=>{
+      res.write(`page hit count is  : ${num} `); //write a response to the client
+      res.end(); //end the response
+    })
+  }
+  
 }).listen(8000);
