6a683d867aeda1ee68b7464ba26d1e6b54a8e125
Mahendra Hegde <hegde.mahendra@tcs.com>
2018-12-03 17:46:43 +0530

    added mongo-kube support

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const http = require('http');
-var redis = require("redis"),
-    client = redis.createClient(6379, 'redis');
-const port=8000;
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://mongo:27017';
+const dbName = 'test';
+const port = process.env.PORT || 8000;
+let db = null;
+let collection = null;
+MongoClient.connect(url, function (err, client) {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
+    collection = db.collection('hitcount');
+    collection.find({}).toArray((err, docs) => {
+      if (docs.length <= 0) {
+        collection.insertMany([{
+          hit: 1
+        }]);
+      }
+    });
+  }
+});
 http.createServer(function (req, res) {
-  if(req.url=='/'){
-    client.incr("hit",(err,num)=>{
-      res.write(`page hit count is  : ${num} `); //write a response to the client
+  if (req.url == '/') {
+
+    collection.findOneAndUpdate({}, {
+      $inc: {
+        hit: 1
+      }
+    }, {
+      new: true
+    }, (err, result) => {
+      res.write(`page hit count is  : ${result.value.hit} `); //write a response to the client
       res.end(); //end the response
-    })
+    });
   }
   console.log(`app is running at port ${port}`)
 }).listen(port);
